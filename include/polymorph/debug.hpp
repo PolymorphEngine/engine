54db0f8e7ae07f00b911eab67fd279a4f8b23e5f
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-23 17:36:22 +0100

    feat(debug): added Logger and ExceptionLogger

---
+++ include/polymorph/debug.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** debug.hpp
+** File description:
+** header for debug.c
+*/
+
+
+#pragma once
+
+#include "polymorph/debug/Logger.hpp"
+
+// Exception
+#include "polymorph/debug/exception/ExceptionLogger.hpp"
---
+++ include/polymorph/debug/exception/ExceptionLogger.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2020
+** ExceptionLogger.hpp
+** File description:
+** header for ExceptionLogger.c
+*/
+
+
+#pragma once
+#include <exception>
+#include <string>
+#include "polymorph/debug/Logger.hpp"
+
+namespace polymorph::engine::debug
+{
+    class ExceptionLogger : public std::exception
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+
+        public:
+            explicit ExceptionLogger(std::string message, Logger::severity level = Logger::severity::MAJOR);
+
+            ///////////////////////////--------------------------///////////////////////////
+
+
+
+            ///////////////////////////////// Properties /////////////////////////////////
+
+        private:
+            std::string _message;
+            Logger::severity _level;
+
+            ///////////////////////////--------------------------///////////////////////////
+
+
+
+            ///////////////////////////////// Methods /////////////////////////////////
+
+        public:
+            const char *what() const noexcept override;
+
+            ///////////////////////////--------------------------///////////////////////////
+
+    };
+}

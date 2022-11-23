0cb531ec4bc4e96858b681e4255fbb99cd968b7e
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-23 16:40:35 +0100

    feat(safe_ptr): added safe_ptr and safe_vector

---
+++ include/polymorph/types/safeptr/safe_vector.hpp
@@ -0,0 +1,71 @@
+/*
+** EPITECH PROJECT, 2020
+** safe_vector.hpp
+** File description:
+** header for safe_vector.c
+*/
+
+
+#pragma once
+#include <vector>
+#include "safe_ptr.hpp"
+
+namespace polymorph::engine
+{
+    template<class T>
+    class safe_vector final : public std::vector<safe_ptr<T>>
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+        public:
+            safe_vector() : std::vector<safe_ptr<T>>()
+            {};
+            
+            safe_vector(std::vector<safe_ptr<T>> data): std::vector<safe_ptr<T>>(data)
+            {};
+            
+            safe_vector(int size) : std::vector<safe_ptr<T>>(size)
+            {};
+            safe_vector(int size, safe_ptr<T> data) : std::vector<safe_ptr<T>>(size, data)
+            {};
+            ~safe_vector() final = default; 
+
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+///////////////////////////// PROPERTIES ////////////////////////////////
+        public:
+
+
+        private:
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+/////////////////////////////// METHODS /////////////////////////////////
+        public:
+            /**
+             * @brief Remove all expired weak_ptr from the vector
+             */
+            void clean() {
+                auto it = std::find_if(this->begin(), this->end(),
+                                       [](safe_ptr<T> instance) { return !instance; });
+                while (it != this->end())
+                {
+                    this->erase(it);
+                    it = std::find_if(this->begin(), this->end(),
+                                      [](safe_ptr<T> instance) { return !instance; });
+                }
+            }
+
+
+        private:
+
+//////////////////////--------------------------/////////////////////////
+
+    };
+
+} // polymorph::engine

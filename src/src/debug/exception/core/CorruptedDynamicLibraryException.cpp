c548ea2be527ba5348ae51a17c8d50ca79714973
Alexandre jublot <alexjublot@gmail.com>
2022-11-24 08:48:43 +0100

    fix(debug): created proper exception for handling open error for dynamic libraries

---
+++ src/src/debug/exception/core/CorruptedDynamicLibraryException.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** DynamicLoaderException.cpp
+** File description:
+** header for DynamicLoaderException.c
+*/
+
+#include "polymorph/debug/exception/core/CorruptedDynamicLibraryException.hpp"
+
+
+polymorph::engine::debug::CorruptedDynamicLibraryException::CorruptedDynamicLibraryException(std::string libPath, std::string error, Logger::severity level)
+        : CoreException("Cannot open dynamic library at path : " + libPath + ", error : " + error, level)
+{
+
+}

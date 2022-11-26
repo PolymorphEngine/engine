7e3efd776d74a54cfd6627e5e078e0529bc2f7db
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-26 17:05:46 +0100

    feat(config): added Xml Scene

---
+++ include/polymorph/config/XmlScene.hpp
@@ -0,0 +1,72 @@
+/*
+** EPITECH PROJECT, 2020
+** XmlScene.hpp
+** File description:
+** header for XmlScene.c
+*/
+
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include "polymorph/debug/Logger.hpp"
+
+namespace myxmlpp {
+    class Doc;
+    class Node;
+}
+
+namespace polymorph::engine::config
+{
+
+    class XmlScene
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+        public:
+            XmlScene(std::shared_ptr<myxmlpp::Node> &scene, debug::Logger& logger, std::string &path);
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+///////////////////////////// PROPERTIES ////////////////////////////////
+        public:
+
+
+        private:
+            std::shared_ptr<myxmlpp::Node> _projectNode;
+            debug::Logger& _logger;
+            std::string _path;
+            std::string _id;
+            std::string _name;
+            std::shared_ptr<myxmlpp::Doc> _sceneDoc;
+            bool _first;
+            std::shared_ptr<myxmlpp::Node> _entities;
+
+//////////////////////--------------------------/////////////////////////
+
+
+
+/////////////////////////////// METHODS /////////////////////////////////
+        public:
+            std::string getId();
+
+            std::string getName();
+
+            bool isFirst() const;
+
+            std::shared_ptr<myxmlpp::Node> getEntities();
+            
+
+
+        private:
+            void _loadEntities();
+
+//////////////////////--------------------------/////////////////////////
+
+    };
+
+} // config

65ff90c6fce31ef7e1eac187c3158b7d9835a605
Alexandre jublot <alexjublot@gmail.com>
2022-11-26 12:15:32 +0100

    docs(types): added doxygen documentation for enable_safe_from_this class

--- include/polymorph/types/safe/enable_safe_from_this.hpp
+++ include/polymorph/types/safe/enable_safe_from_this.hpp
@@ -13,17 +13,29 @@
 
 namespace polymorph::engine
 {
+    /**
+     * @brief Helper class to create safe_ptr from this in inherited classes
+     * @tparam T Type of the inherited class
+     */
     template <typename T>
     class enable_safe_from_this : private std::enable_shared_from_this<T>
     {
 
 /////////////////////////////// METHODS /////////////////////////////////
         public:
+            /**
+             * @brief Get a safe_ptr to the current object
+             * @return safe_ptr<T> to the current object
+             */
             safe_ptr<T> safe_from_this() const
             {
                 return safe_ptr<T>(this->shared_from_this());
             }
 
+            /**
+             * @brief Get a shared_ptr to the current object
+             * @return shared_ptr<T> to the current object
+             */
             std::shared_ptr<T> shared_from_this() const
             {
                 return std::enable_shared_from_this<T>::shared_from_this();

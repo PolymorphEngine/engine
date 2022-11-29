3d3cda1c5cb39818fcb70f39b25b1083aa9b9d3c
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-29 21:43:50 +0100

    feat(api): added documentation for serializable object

--- include/polymorph/api/scripting/ASerializableObject.hpp
+++ include/polymorph/api/scripting/ASerializableObject.hpp
@@ -49,34 +49,128 @@ namespace polymorph::engine::api
 
 /////////////////////////////// METHODS /////////////////////////////////
         public:
+            
+            /**
+             * @brief Virtual Pure method to be implemented in any SerializableObject implementation
+             * that will be called when the object is loaded from a file to set all properties
+             */
             virtual void build() = 0;
+
+            /**
+             * @brief Virtual Pure method to be implemented in any SerializableObject implementation
+             * that will be called when the object is Saved to save all properties
+             */
+            virtual void saveAll() = 0;
+            
+            /**
+             * @brief Returns whether the object is currently stored in a component or in a plugin config file
+             * 
+             * @return True if the object is stored in a plugin config file, false otherwise
+             */
             bool isFromConfig() const;
+            
+            /**
+             * @brief Returns the type of the serializable object
+             * @return The string type of the object
+             */
             std::string getType() const;
             
+            /**
+             * @brief Returns the Component that owns the serializable object
+             * @tparam T The component type
+             * @return The casted safe_ptr of the Component
+             */
             template<typename T>
             safe_ptr<T> getComponent() const 
             {
                 return safe_ptr<T>(std::dynamic_pointer_cast<T>(*_component));
             }
-            
+
+            /**
+             * @brief Returns the PluginConfig that owns the serializable object
+             * @tparam T The PluginConfig type
+             * @return The casted safe_ptr of the PluginConfig
+             */
             template<typename T>
             safe_ptr<T> getPluginConfig() const
             {
                 return safe_ptr<T>(std::dynamic_pointer_cast<T>(*_config));
             }
             
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+
+            template<typename T>
+            void saveProperty(std::string propertyName, T &toSave)
+            {
+                manager.XmlPropertyManager::save(propertyName, toSave);
+            }
+
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+            template<typename T>
+            void saveProperty(std::string propertyName, std::shared_ptr<T> &toSave)
+            {
+                manager.save(propertyName, toSave);
+            }
+
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+            template<typename T>
+            void saveProperty(std::string propertyName, safe_ptr<T> &toSave)
+            {
+                manager.save(propertyName, toSave);
+            }
 
         protected:
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
             template<typename T>
             void setProperty(const std::string &propertyName, std::shared_ptr<T> &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
             {
                 manager.set(propertyName, toSet, level);
             }
+
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
+            template<typename T>
+            void setProperty(const std::string &propertyName, safe_ptr<T> &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
+            {
+                manager.set(propertyName, toSet, level);
+            }
+
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
             template<typename T>
             void setProperty(const std::string &propertyName, T &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
             {
                 manager.XmlPropertyManager::set(propertyName, toSet, level);
             }
+
 //////////////////////--------------------------/////////////////////////
 
     };

40d9ae1e0c752cb94807c4c2de8401b2b2a601e1
Tristan Gremont <tristan.gremont@epitech.eu>
2022-11-30 15:21:51 +0100

    feat(api): implemented APluginConfig

--- include/polymorph/api/plugin/APluginConfig.hpp
+++ include/polymorph/api/plugin/APluginConfig.hpp
@@ -9,46 +9,154 @@
 #pragma once
 
 
-namespace polymorph
-{
-    namespace engine
-    {
-        namespace api
-        {
+#include "myxmlpp/Doc.hpp"
+#include "polymorph/config/XmlPluginConfig.hpp"
 
-            class APluginConfig
-            {
+namespace polymorph::engine {
+    class Engine;
+    
+    namespace debug {
+        class Logger;
+    }
+    namespace time {
+        class Time;
+    }
+}
+namespace polymorph::engine::api
+{
+    class SceneManager;
+    class AssetManager;
+    class PluginManager;
+    
+    class APluginConfig
+    {
 
 ////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
-                public:
-                    APluginConfig();
-
-                    ~APluginConfig();
+        public:
+            APluginConfig(Engine &gameEngine, std::string filePath);
 
 //////////////////////--------------------------/////////////////////////
 
 
 
 ///////////////////////////// PROPERTIES ////////////////////////////////
-                public:
+        public:
+            debug::Logger &Debug;
+            time::Time &time;
+            SceneManager &Scene;
+            AssetManager &Resource;
+            PluginManager &Plugin;
+            Engine &Game;
+            
 
 
-                private:
+        protected:
+            std::shared_ptr<config::XmlPluginConfig> manager;
+
+        private:
+            std::string _filePath;
+            myxmlpp::Doc _doc;
 
 //////////////////////--------------------------/////////////////////////
 
 
 
 /////////////////////////////// METHODS /////////////////////////////////
-                public:
+        public:
+
+            /**
+             * @brief Virtual Pure method to be implemented in any Plugin Config implementation
+             * that will be called when the object is loaded from a file to set all properties
+             */
+            virtual void build() = 0;
+
+            /**
+             * @brief Virtual Pure method to be implemented in any Plugin Config implementation
+             * that will be called when the object is Saved to save all properties
+             */
+            virtual void saveAll() = 0;
+            
+            /**
+             * @brief Save the config to a file
+             * @param saveAllProperties if true, will call saveAll() before saving
+             * @param filePath the path to the file to save to (if empty, will use the path given in the constructor)
+             */
+            void save(bool saveAllProperties = false, std::string filePath = "");
+
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+            template<typename T>
+            void saveProperty(std::string propertyName, T &toSave)
+            {
+                manager->XmlPropertyManager::save(propertyName, toSave);
+            }
+
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+            template<typename T>
+            void saveProperty(std::string propertyName, std::shared_ptr<T> &toSave)
+            {
+                manager->save(propertyName, toSave);
+            }
+
+            /**
+             * @brief Saves a property in the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The value of the property
+             */
+            template<typename T>
+            void saveProperty(std::string propertyName, safe_ptr<T> &toSave)
+            {
+                manager->save(propertyName, toSave);
+            }
 
 
-                private:
+        protected:
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
+            template<typename T>
+            void _setProperty(const std::string &propertyName, std::shared_ptr<T> &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
+            {
+                manager->set(propertyName, toSet, level);
+            }
 
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
+            template<typename T>
+            void _setProperty(const std::string &propertyName, safe_ptr<T> &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
+            {
+                manager->set(propertyName, toSet, level);
+            }
+
+            /**
+             * @brief Sets a property from the Xml data by its name
+             * @param name The name of the property
+             * @tparam T The type of the property
+             * @param value The property to set
+             */
+            template<typename T>
+            void _setProperty(const std::string &propertyName, T &toSet, debug::Logger::severity level = debug::Logger::DEBUG)
+            {
+                manager->XmlPropertyManager::set(propertyName, toSet, level);
+            }
 //////////////////////--------------------------/////////////////////////
+    };
 
-            };
-
-        } // polymorph
-    } // engine
 } // api
--- src/src/api/plugin/APluginConfig.cpp
+++ src/src/api/plugin/APluginConfig.cpp
@@ -6,8 +6,29 @@
 */
 
 #include "polymorph/api/plugin/APluginConfig.hpp"
+#include "polymorph/debug/exception/config/MissingFileException.hpp"
 
 namespace polymorph::engine::api
 {
 
+    void APluginConfig::save(bool saveAllPropeties, std::string filePath)
+    {
+        if (saveAllPropeties)
+            saveAll();
+        manager->saveConfig(filePath);
+    }
+
+    APluginConfig::APluginConfig(Engine &gameEngine, std::string filePath)
+    : _filePath(filePath), Debug(gameEngine.Debug), time(gameEngine.time), Scene(gameEngine.Scene), Resource(gameEngine.Resource), Plugin(gameEngine.Plugin), Game(gameEngine)
+    {
+        std::shared_ptr<myxmlpp::Doc> doc;
+
+        try {
+            doc = std::make_shared<myxmlpp::Doc>(filePath);
+        } catch (myxmlpp::Exception &e) {
+            throw debug::MissingFileException(filePath);
+        }
+
+        manager = std::make_shared<config::XmlPluginConfig>(doc, gameEngine);
+    }
 } // api

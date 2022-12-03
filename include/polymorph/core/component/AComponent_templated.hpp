dd39e3acc8f9ba45e3c9cc0a542064f75e9eb592
Alexandre jublot <alexjublot@gmail.com>
2022-12-03 17:01:17 +0100

    fix(core): removed useless method in AComponent

--- include/polymorph/core/component/AComponent_templated.hpp
+++ include/polymorph/core/component/AComponent_templated.hpp
@@ -116,10 +116,4 @@ namespace polymorph::engine
         _xmlConfig->XmlPropertyManager::set(propertyName, toSet, level);
     }
 
-    template<class T>
-    polymorph::engine::safe_ptr<T> polymorph::engine::AComponent::_self()
-    {
-        return polymorph::engine::safe_ptr<T>(std::dynamic_pointer_cast<T>(shared_from_this()));
-    }
-
 }
